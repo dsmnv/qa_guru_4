963d43e201a74ac745b8cc1e8a5b0cceec964255
dsmnv <dmitry.d.sem@gmail.com>
2022-09-17 13:56:34 +0300

    переименовал функцию names_args в print_name_function

--- homework.py
+++ homework.py
@@ -3,7 +3,7 @@
 # Вызовите ее внутри функций, описанных ниже
 # Подсказка: Имя функции можно получить с помощью func.__name__
 
-def names_args(func, *args):
+def print_name_function(func, *args):
     func = func.__name__.capitalize().replace('_', ' ')
     print('Function name:', func)
     print('Function args:', *args)
@@ -12,15 +12,15 @@ def names_args(func, *args):
 
 
 def open_browser(browser_name):
-    names_args(open_browser, browser_name)
+    print_name_function(open_browser, browser_name)
 
 
 def go_to_companyname_homepage(page_url):
-    names_args(go_to_companyname_homepage, page_url)
+    print_name_function(go_to_companyname_homepage, page_url)
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    names_args(find_registration_button_on_login_page, page_url, button_text)
+    print_name_function(find_registration_button_on_login_page, page_url, button_text)
 
 
 
